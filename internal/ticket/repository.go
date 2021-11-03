d3ed5f942c0698c52b5ea6b119057c3a296a6600
404 <ivan@test.it>
2021-11-04 00:58:47 +0300

    ref: external db pool init

--- internal/ticket/repository.go
+++ internal/ticket/repository.go
@@ -7,7 +7,7 @@ import (
 
 // Repository ...
 type Repository interface {
-	Create(ctx context.Context, arg *Ticket) (id string, err error)
+	Create(ctx context.Context, arg *Ticket) (ticketID uuid.UUID, err error)
 	List(ctx context.Context, f *Filter) ([]*Ticket, error)
 	Read(ctx context.Context, ticketID uuid.UUID) (*Ticket, error)
 	Update(ctx context.Context, ticket *Ticket) error
--- internal/ticket/repository/postgres/postgress.go
+++ internal/ticket/repository/postgres/postgress.go
@@ -2,16 +2,13 @@ package postgres
 
 import (
 	"context"
-	"fmt"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/rodkevich/gmp-tickets/internal/configs"
+	"github.com/rodkevich/gmp-tickets/internal/ticket"
 	"log"
 	"math/rand"
 	"time"
-
-	"github.com/google/uuid"
-	"github.com/jackc/pgx/v4/pgxpool"
-
-	"github.com/rodkevich/gmp-tickets/internal/configs"
-	"github.com/rodkevich/gmp-tickets/internal/ticket"
 )
 
 type datasource struct {
@@ -32,21 +29,29 @@ const (
 // VALUES (DEFAULT, 'test_ticket', 'test_ticket_full_name', 'test_ticket_description',
 // 'active', 1, 22, 333.33, 251, DEFAULT, null, null, null);
 
-const InsertNewTicket = `
-INSERT INTO tickets
-(name, full_name, description, status, owner_id, amount, price, currency)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-RETURNING id
+const InsertNewPhoto = `
+INSERT INTO public.photo (id, ticket_id, is_main, presented, mime_type, size_kb)
+VALUES (
+DEFAULT, $1, DEFAULT, DEFAULT, $2, $3);
 `
 
-func (d *datasource) Create(ctx context.Context, arg *ticket.Ticket) (id string, err error) {
+const InsertNewTicket = `
+INSERT INTO ticket
+(name, full_name, description, status, owner_id, amount, price, currency)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+RETURNING id;
+`
+
+func (d *datasource) Create(ctx context.Context, arg *ticket.Ticket) (id uuid.UUID, err error) {
+
 	tx, err := d.db.Begin(ctx)
 	if err != nil {
+		log.Println(err)
 		return
 	}
 	defer tx.Rollback(ctx)
-
-	row := tx.QueryRow(ctx, InsertNewTicket,
+	err = tx.QueryRow(
+		ctx, InsertNewTicket,
 		arg.Name,
 		arg.FullName,
 		arg.Description,
@@ -54,10 +59,12 @@ func (d *datasource) Create(ctx context.Context, arg *ticket.Ticket) (id string,
 		arg.OwnerID,
 		arg.Amount,
 		arg.Price,
-		arg.Currency,
-	)
-	err = row.Scan(&id)
-
+		arg.Currency).Scan(&id)
+	err = tx.Commit(ctx)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	return
 }
 
@@ -82,32 +89,11 @@ func (d *datasource) Search(ctx context.Context, req *ticket.Filter) ([]*ticket.
 }
 
 func (d datasource) String() string {
-	return "Postgres"
+	return "Ticket Postgres"
 }
 
-func NewDatasource(cfg *configs.Configs) (ticket.Repository, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+func NewDatasource(cfg *configs.Configs, pool *pgxpool.Pool) (ticket.Repository, error) {
 
-	var connString string
-	connString = fmt.Sprintf(
-		"%s://%s/%s?sslmode=%s&user=%s&password=%s&pool_max_conns=%v",
-		cfg.Database.Driver,
-		cfg.Database.Host,
-		cfg.Database.Name,
-		cfg.Database.SslMode,
-		cfg.Database.User,
-		cfg.Database.Pass,
-		cfg.Database.MaxPoolConnections,
-	)
-
-	pool, err := pgxpool.Connect(ctx, connString)
-
-	if err != nil {
-		log.Printf("Unable to connect database: %v\n", err)
-		return nil, err
-	}
-	log.Printf("New PG datasource connected to: %v", connString)
 	for {
 		_, err := pool.Exec(context.Background(), "SELECT '"+cfg.Api.Name+".ticket'::regclass")
 		if err == nil {
@@ -130,7 +116,7 @@ func NewDatasource(cfg *configs.Configs) (ticket.Repository, error) {
 			_, err := pool.Exec(context.Background(), "SELECT '"+cfg.Api.Name+".ticket'::regclass")
 			if err == nil {
 				log.Println("Database is ready")
-				return nil, err
+				return &datasource{pool}, nil
 			}
 		}
 	}
