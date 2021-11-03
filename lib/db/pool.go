55564beacfe273dac03d2e346ed5b2500d38c89c
404 <ivan@test.it>
2021-11-03 19:45:53 +0300

    ref: extract pool init

---
+++ lib/db/pool.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/rodkevich/gmp-tickets/internal/configs"
+)
+
+func NewConnectionPool(ctx context.Context, cfg *configs.Configs) (pool *pgxpool.Pool, err error) {
+
+	var connString string
+	connString = fmt.Sprintf(
+		"%s://%s/%s?sslmode=%s&user=%s&password=%s&pool_max_conns=%v",
+		cfg.Database.Driver,
+		cfg.Database.Host,
+		cfg.Database.Name,
+		cfg.Database.SslMode,
+		cfg.Database.User,
+		cfg.Database.Pass,
+		cfg.Database.MaxPoolConnections,
+	)
+
+	pool, err = pgxpool.Connect(ctx, connString)
+
+	if err != nil {
+		log.Printf("Unable to connect database: %v\n", err)
+		return nil, err
+	}
+	log.Printf("New PG datasource connected to: %v", connString)
+	return pool, nil
+}
