8602b1ad7a6915626be799c0fd5030b92e1ae5e9
404 <ivan@test.it>
2021-11-05 14:05:53 +0300

    fix: rm comment

--- internal/configs/api.go
+++ internal/configs/api.go
@@ -27,7 +27,7 @@ type ApiConfig struct {
 }
 
 var defaultApiCfg = ApiConfig{
-	Name:              "gmp-tickets", // should match db schema
+	Name:              "gmp-tickets",
 	Host:              net.ParseIP("0.0.0.0"),
 	Port:              "12300",
 	ReadTimeout:       time.Duration(5) * time.Second,
