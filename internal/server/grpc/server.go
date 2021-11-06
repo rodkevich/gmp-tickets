998f188327f0268c1e43492673b887b7ed086eef
404 <ivan@test.it>
2021-11-06 04:51:21 +0300

    fix: grpc stubs

--- internal/server/grpc/server.go
+++ internal/server/grpc/server.go
@@ -1,20 +1,67 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
-	"google.golang.org/grpc"
+	googleGRPC "google.golang.org/grpc"
 
-	userGRPC "github.com/rodkevich/gmp-tickets/internal/user/grpc"
+	"github.com/rodkevich/gmp-tickets/internal/user/grpc"
 )
 
 // GRPCServer base type
 type GRPCServer struct {
-	userGRPC.UnimplementedUserServiceServer
+	grpc.UnimplementedUserServiceServer
+}
 
-	// db
+func (s *GRPCServer) CreateUser(ctx context.Context, request *grpc.CreateUserRequest) (*grpc.CreateUserResponse, error) {
+
+	return &grpc.CreateUserResponse{Token: "may be sessions ... ?"}, nil
+}
+
+func (s *GRPCServer) ReadUser(ctx context.Context, request *grpc.ReadUserRequest) (*grpc.ReadUserResponse, error) {
+
+	return &grpc.ReadUserResponse{}, nil
+}
+
+func (s *GRPCServer) UpdateUser(ctx context.Context, request *grpc.UpdateUserRequest) (*grpc.ReadUserResponse, error) {
+	return &grpc.ReadUserResponse{User: nil}, nil
+}
+
+func (s *GRPCServer) DeleteUser(ctx context.Context, request *grpc.DeleteUserRequest) (*grpc.DeleteUserResponse, error) {
+
+	return &grpc.DeleteUserResponse{}, nil
+}
+
+func (s *GRPCServer) Login(ctx context.Context, request *grpc.LoginRequest) (*grpc.LoginResponse, error) {
+	return &grpc.LoginResponse{Token: "may be sessions ... ?"}, nil
+}
+
+func (s *GRPCServer) Logout(ctx context.Context, request *grpc.LogoutRequest) (*grpc.LogoutResponse, error) {
+
+	return &grpc.LogoutResponse{}, nil // may be outdated token ?
+}
+
+func (s *GRPCServer) CreateProfile(ctx context.Context, request *grpc.CreateProfileRequest) (*grpc.CreateProfileResponse, error) {
+
+	return &grpc.CreateProfileResponse{}, nil
+}
+
+func (s *GRPCServer) ReadProfile(ctx context.Context, request *grpc.ReadProfileRequest) (*grpc.ReadProfileResponse, error) {
+
+	return &grpc.ReadProfileResponse{}, nil
+}
+
+func (s *GRPCServer) UpdateProfile(ctx context.Context, request *grpc.UpdateProfileRequest) (*grpc.ReadProfileResponse, error) {
+
+	return &grpc.ReadProfileResponse{}, nil
+}
+
+func (s *GRPCServer) DeleteProfile(ctx context.Context, request *grpc.DeleteProfileRequest) (*grpc.DeleteProfileResponse, error) {
+
+	return &grpc.DeleteProfileResponse{}, nil
 }
 
 // Run start a server
@@ -22,11 +69,11 @@ func (s *GRPCServer) Run(address string) (net.Listener, error) {
 	return net.Listen("tcp", address)
 }
 func main() {
-	startApp("127.0.0.1:9090")
+	startApp("127.0.0.1:12312")
 }
 
 func startApp(address string) {
-	s := grpc.NewServer()
+	s := googleGRPC.NewServer()
 
 	grpcUsersServer := &GRPCServer{}
 	// init fake database
@@ -34,14 +81,14 @@ func startApp(address string) {
 	// 	log.Fatal(err)
 	// }
 	// register services
-	userGRPC.RegisterUserServiceServer(s, grpcUsersServer)
+	grpc.RegisterUserServiceServer(s, grpcUsersServer)
 
 	usersInstance, err := grpcUsersServer.Run(address)
 	fmt.Println("Server is running")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// serve grpc
+	// serve googleGRPC
 	if err := s.Serve(usersInstance); err != nil {
 		log.Fatal(err)
 	}
